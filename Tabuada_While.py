17000b1a4a53182ee5a8a9ead7fbcb021a412c58
William Lima <107055555+Emboava2022@users.noreply.github.com>
2022-11-18 14:36:01 -0300

    Tabuada simplificada utilizando While
    Tabuada utilizando While - simplificando o conhecimento adquirido em Python 😉

---
+++ Tabuada_While.py
@@ -0,0 +1,14 @@
+# Tabuada com Python
+
+
+x = 0
+valor = (int(input("Digite o valor: ")))
+print(f"O valor digitado foi {valor}\n")
+
+while x < 11:
+
+    print(f"A tabuada é: {valor} X {x} = ", valor * x)
+    x += 1
+
+
+
